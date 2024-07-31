e52a78f82e74a0d2b8da12ba529acf377a0ba0a9
thenotoriousob <32715357+thenotoriousob@users.noreply.github.com>
2024-07-31 19:24:28 +0100

    Change modal to a dialog

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import Dog from "./Dog.js";
 
 const dogProfiles = [];
 const user = new Dog("Shenanigans", "images/dog-shenanigans.png", 4, "Shenanigans by name, shenanigans by nature");
-const profileModal = document.getElementById("user-profile-modal");
+const profileModal = document.querySelector("dialog");
 const profileForm = document.getElementById("user-profile-form");
 const mainEl = document.getElementById("main");
 const likedProfilesEl = document.getElementById("liked-profiles");
@@ -17,30 +17,20 @@ let currentDog = new Dog(dogs[currentDogIndex].name,
 
 document.addEventListener("click", (e) => {
 
-    // Don't want any of the buttons to work if the modal is open
-    if (profileModal.style.display != 'block') {
-
-        if (e.target.id === "btn-like") {
-            handleSwipedClick("liked");
-        }
-        else if (e.target.id === "btn-dislike") {
-            handleSwipedClick("disliked");
-        }
-        else if (e.target.id === "logo-btn") {
-            handleLogoClick();
-        }
-        else if(e.target.id === "chat-btn") {
-            renderLikedDogs();
-        }
-        else if(e.target.id === "profile-btn") {
-            openOrCloseModal("block");
-      }
+    if (e.target.id === "btn-like") {
+        handleSwipedClick("liked");
     }
-    else {
-        // If the user clicks outside of the modal then close it
-        if(!e.target.closest('.user-profile-container')) {
-            openOrCloseModal("none");
-        }
+    else if (e.target.id === "btn-dislike") {
+        handleSwipedClick("disliked");
+    }
+    else if (e.target.id === "logo-btn") {
+        handleLogoClick();
+    }
+    else if(e.target.id === "chat-btn") {
+        renderLikedDogs();
+    }
+    else if(e.target.id === "profile-btn") {
+        openOrCloseModal("block");
     }
 
 });
@@ -85,7 +75,13 @@ function handleLogoClick() {
 
 function openOrCloseModal(display) {
 
-    profileModal.style.display = display;
+    // profileModal.style.display = display;
+
+    if (display === "none") {
+        profileModal.close();
+    } else {
+        profileModal.showModal();
+    }
 
     mainEl.classList.toggle("blur-background");
 
@@ -99,7 +95,7 @@ function handleProfileSubmit() {
     const bio = profileFormData.get('bio');
     const avatar = profileFormData.get('avatar-loc');
 
-    profileModal.style.display = "";
+    // profileModal.style.display = "";
 
     profileForm.reset();
 
